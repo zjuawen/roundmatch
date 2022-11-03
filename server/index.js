9ab494672766ee73e20dae4631e3200edd86218b
awen <zjuawen@163.com>
2022-11-03 17:07:27 +0800

    init self-host server

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const session = require('express-session');
+const bodyParser = require("body-parser");
+const fileupload = require("express-fileupload");
+const cors = require("cors");
+const server = express();
+// const db = require("./models");
+
+const corsSettings = {
+	origin: true,
+	// origin: ["https://roundmatch.microripples.cn", "https://medical.tattoosoft.cn:8443", "http://localhost:8080"],
+	credentials: true,
+};
+
+const sessionSettings = {
+	secret: 'roundmatch token',
+	resave: true,
+	rolling: true,
+	saveUninitialized: false,
+	name: 'token',
+	cookie: { 
+		maxAge: 60*60000,	// 60 mins
+		// secure: true,
+		// proxy : true
+		// sameSite: 'None',
+	}	
+};
+
+const api = require("./routes/index");
+
+server.use(cors(corsSettings));
+server.use(session(sessionSettings));
+// Parse request of content-type - application/json
+server.use(bodyParser.json());
+// parse requests of content-type -application/x-www-form-urlencoded
+server.use(bodyParser.urlencoded({ extended: true }));
+// parse file uploader
+server.use(fileupload());
+
+server.use("/", api);
+// set listening ports for request
+const port = process.env.PORT || 8300;
+
+server.listen(port, () => {
+  console.log(`Server running on port : ${port} on ${new Date()}`);
+});
+
+// Run following function if you want drop existing tables and re-sync database
+// db.dropRestApiTable();
+
+// db.databaseConf.sync();
---
+++ server/routes/index.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Christos Ploutarchou
+ * Project : node_rest_api_with_mysql
+ * Filename : routes.js
+ * Date: 05/04/2020
+ * Time: 01:45
+ **/
+// const dummy = require("../controllers/Dummy");
+
+// const admin = require("../controllers/Adminuser");
+// const doctor = require("../controllers/Doctor");
+// const patient = require("../controllers/Patient");
+// const data = require("../controllers/Data");
+// const dict = require("../controllers/Dict");
+// const oss = require("../controllers/Oss");
+
+// const wechat = require("../controllers/WechatApi");
+
+const express = require("express");
+const router = express.Router();
+
+// // debug
+// // router.post("/api/test", dummy.test);
+// router.get("/api/test", dummy.test);
+
+// // admin users
+// router.post("/api/admin/login", admin.login);
+// router.get("/api/admin/logout", admin.logout);
+// router.post("/api/admin/create", admin.create);
+// router.get("/api/admin/list", admin.listUser);
+// router.post("/api/admin/update", admin.update);
+// router.get("/api/admin/current", admin.getCurrent);
+
+// // Doctors
+// // router.post("/api/doctor/login", doctor.login);
+// router.post("/api/doctor/create", doctor.create);
+// router.post("/api/doctor/bind", doctor.bind);
+// router.post("/api/doctor/update", doctor.update);
+// router.post("/api/doctor/detail", doctor.getDoctorByID);
+// router.get("/api/doctor/list", doctor.listDoctors);
+// router.post("/api/doctor/bindQrcode", doctor.getBindQrcode);
+// router.post("/api/doctor/inviteQrcode", doctor.getInviteQrcode);
+// // router.post("/api/doctor/saveData", doctor.saveData);
+// router.get("/api/doctor/current", doctor.getCurrent);
+
+// // patient
+// router.post("/api/patient/create", patient.create);
+// router.post("/api/patient/update", patient.update);
+// router.get("/api/patient/list", patient.listPatients);
+// router.get("/api/patient/current", patient.getCurrent);
+// router.post("/api/patient/urge", wechat.urge);
+
+// // data record
+// router.post("/api/data/save", data.saveData);
+// router.post("/api/data/update", data.update);
+// router.get("/api/data/list", data.listData);
+// router.get("/api/data/detail", data.load);
+// router.get("/api/data/back", data.goback);
+// router.post("/api/data/stat", data.statistics);
+// router.get("/api/data/stageData", data.getStageData);
+// router.get("/api/data/exportStageData", data.exportStageData);
+
+// // utils
+// router.post("/api/user/login", wechat.login);
+// router.post("/api/oss/upload", oss.upload);
+// router.get("/api/oss/get", oss.getUrl);
+// router.get("/api/dict", dict.read);
+// router.post("/api/oss/urltopdf", oss.saveUrlToPDF);
+
+
+module.exports = router;
